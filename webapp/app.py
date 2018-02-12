b164b70acbd4778e8b1aaf5e44d9585b08c6baec
Vaida Plankyte <vaidaplankyte@gmail.com>
2018-02-12 15:32:27 +0000

    WIP full array and individual next job code

--- webapp/app.py
+++ webapp/app.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+global desks, job_queue, sched_alg, next_job
+
 desks = {
     1 : {'name' : 'Desk 1', 'colour' : 'red'},
     2 : {'name' : 'Desk 2', 'colour' : 'yellow'},
@@ -13,13 +15,14 @@ desks = {
     6 : {'name' : 'Desk 6', 'colour' : 'purple'}
 }
 
-job_queue = []
+#TODO: temporary edit
+job_queue = [5, 1, 3, 4]
 
 error_msg = False
 
 sched_alg = "none"
 
-current_job = 0
+next_job = None
 
 @app.route("/")
 def index():
@@ -48,11 +51,10 @@ def action(desk, action):
             message = " OfficeBot has been called to " + deskName + "."
             error_msg = False
 
-            current_job = desk
             # add desk to front of job_queue
             job_queue.insert(0, desk)
-            reorder_jobs()
             print("\nDEBUG:\ncall " + str(desk) + "\njob_queue is: " + str(job_queue) + "\n")
+            reorder_jobs()
 
         # multiple calls to the same location are ignored
         else:
@@ -82,18 +84,61 @@ def reorder_jobs():
     #         |
     #    1 ---|
     #
-    pass
+    write_queue()
 
-def send_job():
-    # Gets called by logic processor
+def write_queue():
+    #TODO: is read/write going to cause sync issues when used with logic_draft
 
-    # If there are no jobs in the queue
-    if (len(job_queue) == 0):
-        return -1
+    # If there are jobs in the queue
+    if (len(job_queue) > 0):
 
-    current_job = job_queue.pop()
-    reorder_jobs()
-    return current_job
+        # process file, check if any items were removed
+        # remove them from the job_queue
+        # put the new queue in (if still not empty)
+
+        # Open file
+        file = open("dest.txt","r")
+        content = file.read()
+        file.close()
+
+        # Convert content to list of ints
+        content = content.split()
+        content = list(map(int, content))
+        print(content)
+
+
+
+    else:
+        print("write_queue was called, but there are no jobs in job_queue.")
+
+        # Check if job_queue
+
+        # # Check if file is empty (logic has taken a destination to process)
+        # file = open("dest.txt","r")
+        # content = file.read()
+        # file.close()
+        # print("Content of file is: " + content)
+        # # TODO: Might cause issues if logic pulls information between read &
+        # # write here (inaccurate data)
+        #
+        # # If file contains a destination still:
+        # if (len(content) > 0):
+        #     next_job = job_queue[-1]
+        #     file = open("dest.txt","w")
+        #     file.write(str(next_job))
+        #     file.close()
+        #     print("Overwritten new destination: " + str(next_job) + ".")
+        # # Else, the destination is being processed and should be removed
+        # # from the job_queue.
+        # else:
+            # Check needed as on initialisation, dest.txt is empty and
+            # next_job is None
+            # if (next_job is None):
+            #     job_queue.remove(next_job)
+            #     print("Removed " + next_job + " from job queue.")
+            #     print("job_queue is: " + str(next_job))
+            # else:
+            #     print("File is empty on initialisation, no action taken.")
 
 # redirect unknown URLs to homepage
 @app.errorhandler(404)
