d0c74e8a81a646aa8cdd86c5acc2d3dea081a304
lStewieAl <lStewieAl@users.noreply.github.com>
2018-02-21 18:40:47 +0000

    Replaced global variables with instance variables
    The Line_Follower object contains 2 motor threads, an LED thread and the Ultrasonic/Color sensors.
    The method 'follow_line(color, nextColor, turnSide, numJunctionsToIgnore)' can be run on the object to follow a line given the arguments.

--- EV3/Line_Follower.py
+++ EV3/Line_Follower.py
@@ -3,110 +3,102 @@ from time import sleep
 from ev3dev.auto import *
 import threading
 
+
+# noinspection PyPep8Naming
 class Line_Follower:
-    def __init__(self):
-        ultrasonicSensor = UltrasonicSensor(INPUT_AUTO); assert ultrasonicSensor.connected
-        colorSensorLeft = ColorSensor('in1'); assert colorSensorLeft.connected
-        colorSensorRight = ColorSensor('in4'); assert colorSensorRight.connected
-        button = Button()
+    # define conversion between number and color
+    COLORS = {0: 'None', 1: 'Black', 2: 'Blue', 3: 'Green', 4: 'Yellow', 5: 'Red', 6: 'White', 7: 'Brown'}
 
-    def main(self):
-        # Connect color and ultrasonic sensors and check that they
-        # are connected.
-        global COLORS, l_motor_thread, r_motor_thread, led_thread
+    def __init__(self, speed, lineColorToFollow, nextColor, turnSide):
+        self.lineColorToFollow = lineColorToFollow
+        self.nextColor = nextColor
+        self.turnSide = turnSide
 
-        ultrasonicSensor = UltrasonicSensor(INPUT_AUTO); assert ultrasonicSensor.connected
-        colorSensorLeft = ColorSensor('in1'); assert colorSensorLeft.connected
-        colorSensorRight = ColorSensor('in4'); assert colorSensorRight.connected
+        # initialise motors and LEDs
+        self.l_motor_thread, self.r_motor_thread = self.initialiseMotors(speed)
+        self.led_thread = self.initialiseLEDThread()
 
-        # define conversion between number and color
-        COLORS = {0: 'None', 1: 'Black', 2: 'Blue', 3: 'Green', 4: 'Yellow', 5: 'Red', 6: 'White', 7: 'Brown'}
+        # initialise sensors and check they are connected
+        self.ultrasonicSensor = UltrasonicSensor(INPUT_AUTO); assert self.ultrasonicSensor.connected
+        self.colorSensorLeft = ColorSensor('in1'); assert self.colorSensorLeft.connected
+        self.colorSensorRight = ColorSensor('in4'); assert self.colorSensorRight.connected
+        self.button = Button()
 
-        # FOR TESTING PURPOSES TODO remove
-        lineColorToFollow = 5  # red
-        nextColor = 3  # blue
-        turnSide = 'left'
-        speed = 30
-
-        self.follow_line(lineColorToFollow, nextColor, speed, turnSide)
-
-
-    def follow_line(self, colorToFollow, nextLineColor, speed, sideTurnIsOn):
-        self.initialiseMotors(speed)
-        self.initialiseLEDThread()
-
+    def follow_line(self, colorToFollow, nextLineColor, sideTurnIsOn, junctionsToIgnore):
         # follow line until enter or backspace are pressed
-        while True:
-            if self.button.enter or self.button.backspace:
-                Exception("Exit button was pressed.")
+        try:
+            while True:
+                if self.button.enter or self.button.backspace:
+                    Exception("Exit button was pressed.")
 
-            # if something is close to the sensor, stop motors until it's gone
-            if self.ultrasonicSensor.value() < 90:  # 90mm
-                self.pauseMotorsAndStartLEDFlashing()
+                # if something is close to the sensor, stop motors until it's gone
+                if self.ultrasonicSensor.value() < 90:  # 90mm
+                    self.pauseMotorsAndStartLEDFlashing()
 
-                self.sayImInDanger()
+                    self.say("I am in danger!")
 
-                while self.ultrasonicSensor.value() < 90:
-                    if self.button.enter or self.button.backspace:
-                        Exception("Exit button was pressed.")
+                    while self.ultrasonicSensor.value() < 90:
+                        if self.button.enter or self.button.backspace:
+                            Exception("Exit button was pressed.")
 
-                # while broken indicates obstacle is gone
-                self.unpauseMotorsAndStopLEDFlashing()
+                    # while broken indicates obstacle is gone
+                    self.unpauseMotorsAndStopLEDFlashing()
 
+                # reverse the left wheel if the left colour sensor is colorToFollow
+                if self.colorSensorLeft.color == colorToFollow:
+                    self.l_motor_thread.reverse = True
+                else:
+                    self.l_motor_thread.reverse = False
 
-            # reverse the left wheel if the left colour sensor is colorToFollow
-            if self.colorSensorLeft.color == colorToFollow:
-                l_motor_thread.reverse = True
-            else:
-                l_motor_thread.reverse = False
+                # reverse the right wheel if the right colour sensor is colorToFollow
+                if self.colorSensorRight.color == colorToFollow:
+                    self.r_motor_thread.reverse = True
+                else:
+                    self.r_motor_thread.reverse = False
 
-            # reverse the right wheel if the right colour sensor is colorToFollow
-            if self.colorSensorRight.color == colorToFollow:
-                r_motor_thread.reverse = True
-            else:
-                r_motor_thread.reverse = False
+                # ~100hz
+                sleep(0.01)
 
-            # ~100hz
-            sleep(0.01)
+                # detect next line color
+                if sideTurnIsOn == 'LEFT' and self.colorSensorLeft.color == nextLineColor \
+                        or sideTurnIsOn == 'RIGHT' and self.colorSensorRight.color == nextLineColor:
+                    colorToFollow = nextLineColor
+                    print("\nTurning! New color: " + self.COLORS[colorToFollow])
 
-            # detect next line color
-            if sideTurnIsOn == 'LEFT' and self.colorSensorLeft.color == nextLineColor \
-                    or sideTurnIsOn == 'RIGHT' and self.colorSensorRight.color == nextLineColor:
-                colorToFollow = nextLineColor
-                print("\nTurning! New color: " + COLORS[colorToFollow])
+                self.checkForSpeedChange()
 
-            self.checkForSpeedChange()
+                colorToFollow = self.checkForColorChange(colorToFollow)
+        except Exception("Exit button was pressed."):
+            print("Button was pressed - Stopping motors.")
+            self.l_motor_thread.motor.stop()
+            self.r_motor_thread.motor.stop()
+            sleep(0.5)
 
-            colorToFollow = self.checkForColorChange(colorToFollow)
-
-        print("Button was pressed - Stopping motors.")
-        l_motor_thread.motor.stop()
-        r_motor_thread.motor.stop()
-        sleep(0.5)
-
+    # accept a list of tuples containing the colors, and turnsides to follow
+    def follow_lines(self, list_of_tuples):
+        for (colorToFollow, nextLineColor, turnSide, junctionsToIgnore) in list_of_tuples:
+            self.follow_line(colorToFollow, nextLineColor, turnSide, junctionsToIgnore)
 
     def initialiseMotors(self, speed):
         l_motor_thread = self.MotorThread("LEFT", "A")
-        l_motor_thread.setDaemon(True)  # stop thread when main thread ends
-        l_motor_thread.speed = speed
-        l_motor_thread.start()
+        self.l_motor_thread.setDaemon(True)  # stop thread when main thread ends
+        self.l_motor_thread.speed = speed
+        self.l_motor_thread.start()
 
         r_motor_thread = self.MotorThread("RIGHT", "D")
-        r_motor_thread.setDaemon(True)
-        r_motor_thread.speed = speed
-        r_motor_thread.start()
+        self.r_motor_thread.setDaemon(True)
+        self.r_motor_thread.speed = speed
+        self.r_motor_thread.start()
         print("Starting motors: ")
+        return l_motor_thread, r_motor_thread
 
-
-    ### general classes
     class MotorThread(threading.Thread):
         def __init__(self, side, out):
             self.work = True
             self.pause = False
             self.reverse = False
-            self.speed = 0
-
             self.side = side
+            self.speed = 0
 
             self.motor = LargeMotor(OUTPUT_A)
             if out.upper() == "B":
@@ -127,15 +119,14 @@ class Line_Follower:
                 elif self.reverse:
                     self.motor.run_direct(duty_cycle_sp=-self.speed)
                 else:
-                    self.motor.run_direct(duty_cycle_sp=self)
+                    self.motor.run_direct(duty_cycle_sp=self.speed)
             self.motor.stop()
-
 
     def initialiseLEDThread(self):
         led_thread = self.FlashLEDs()
         led_thread.setDaemon(True)
         led_thread.start()
-
+        return led_thread
 
     class FlashLEDs(threading.Thread):
         def __init__(self):
@@ -156,23 +147,19 @@ class Line_Follower:
                     Leds.set_color(Leds.RIGHT, Leds.RED)
                     sleep(0.300)  # 300ms
 
-
     def pauseMotorsAndStartLEDFlashing(self):
-        l_motor_thread.pause = True
-        r_motor_thread.pause = True
-        led_thread.flashing = True
-
+        self.l_motor_thread.pause = True
+        self.r_motor_thread.pause = True
+        self.led_thread.flashing = True
 
     def unpauseMotorsAndStopLEDFlashing(self):
-        led_thread.flashing = False
-        l_motor_thread.pause = False
-        r_motor_thread.pause = False
+        self.led_thread.flashing = False
+        self.l_motor_thread.pause = False
+        self.r_motor_thread.pause = False
 
-
-    def sayImInDanger(self):
+    def say(self, message):
         Sound.set_volume(100)
-        Sound.speak("I am in danger!")
-
+        Sound.speak(message)
 
     def checkForSpeedChange(self):
         # increase speed with UP button, decrease with Down
@@ -181,29 +168,25 @@ class Line_Follower:
         if self.button.down:
             self.decreaseSpeed()
 
-
     def decreaseSpeed(self):
-        l_motor_thread.speed -= 1
-        r_motor_thread.speed -= 1
-        print("\n" + "Left: " + str(l_motor_thread.speed) + "  -  Right: " + str(r_motor_thread.speed))
-
+        self.l_motor_thread.speed -= 1
+        self.r_motor_thread.speed -= 1
+        print("\n" + "Left: " + str(self.l_motor_thread.speed) + "  -  Right: " + str(self.r_motor_thread.speed))
 
     def increaseSpeed(self):
-        l_motor_thread.speed += 1
-        r_motor_thread.speed += 1
-        print("\n" + "Left: " + str(l_motor_thread.speed) + "  -  Right: " + str(r_motor_thread.speed))
-
+        self.l_motor_thread.speed += 1
+        self.r_motor_thread.speed += 1
+        print("\n" + "Left: " + str(self.l_motor_thread.speed) + "  -  Right: " + str(self.r_motor_thread.speed))
 
     def checkForColorChange(self, colorToFollow):
         # change line color to follow with left/right
         if self.button.left:
-            colorToFollow = (colorToFollow - 1) % len(COLORS)
-            print("\nLine color: " + COLORS[colorToFollow])
+            colorToFollow = (colorToFollow - 1) % len(self.COLORS)
+            print("\nLine color: " + self.COLORS[colorToFollow])
         if self.button.right:
-            colorToFollow = (colorToFollow + 1) % len(COLORS)
-            print("\nLine color: " + COLORS[colorToFollow])
+            colorToFollow = (colorToFollow + 1) % len(self.COLORS)
+            print("\nLine color: " + self.COLORS[colorToFollow])
         return colorToFollow
 
-
     if __name__ == '__main__':
-        main()
+        pass
