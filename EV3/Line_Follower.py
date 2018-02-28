3cddd17e36f452c6e0b755f100d44e60c1fd3405
Alex Stewart <s1437078@janda.inf.ed.ac.uk>
2018-02-28 17:15:27 +0000

    Removed while loop in motor thread for performance

--- EV3/Line_Follower.py
+++ EV3/Line_Follower.py
@@ -3,7 +3,6 @@ from time import sleep
 from ev3dev.auto import *
 import threading
 
-
 # noinspection PyPep8Naming
 class Line_Follower:
     # define conversion between number and color
@@ -13,84 +12,115 @@ class Line_Follower:
     def __init__(self, speed=30):
 
         # initialise motors and LEDs
-        self.l_motor_thread, self.r_motor_thread = self.initialiseMotors(speed)
+        self.leftMotor, self.rightMotor = self.initialiseMotors(speed)
         self.led_thread = self.initialiseLEDThread()
 
         # initialise sensors and check they are connected
-        self.ultrasonicSensor = UltrasonicSensor(INPUT_AUTO); assert self.ultrasonicSensor.connected
+#       self.ultrasonicSensor = UltrasonicSensor(INPUT_AUTO); assert self.ultrasonicSensor.connected
         self.colorSensorLeft = ColorSensor('in1'); assert self.colorSensorLeft.connected
         self.colorSensorRight = ColorSensor('in4'); assert self.colorSensorRight.connected
         self.button = Button()
 
-    def follow_line(self, colorToFollow, nextLineColor, sideTurnIsOn, junctionsToIgnore):
-
+    def follow_line(self, colorToFollow, nextLineColor, sideTurnIsOn, junctionsToIgnore=0):
+        print("Following: " + colorToFollow)
         # convert the string color back into a number (to help with performance)
         colorToFollow = self.COLOR_TO_NUMBER[colorToFollow]
         nextLineColor = self.COLOR_TO_NUMBER[nextLineColor]
 
         # the wheel rotation at the previous junction that was visited
-        leftWheelRotationAtPreviousJunction = self.l_motor_thread.motor.position
-        rightWheelRotationAtPreviousJunction = self.r_motor_thread.motor.position
+        leftWheelRotationAtPreviousJunction = self.leftMotor.motor.position
+        rightWheelRotationAtPreviousJunction = self.rightMotor.motor.position
 
         junctionsToIgnore = int(junctionsToIgnore)
 
+        # start both wheels
+        self.leftMotor.forward()
+        self.rightMotor.forward()
+
+        # reverse wheel by default
+        self.leftMotor.reverse()
+        self.rightMotor.reverse()
+
         # follow line until enter or backspace are pressed
-        try:
-            while True:
-                if self.button.enter or self.button.backspace:
-                    Exception("Exit button was pressed.")
+        while True:
+#            if self.button.enter or self.button.backspace:
+#                print("Button was pressed - Stopping motors.")
+#                self.l_motor_thread.work = False
+#                self.r_motor_thread.work = False
+#                sleep(0.5)
+#                break
+#
+#            # when something is close to the sensor, stop motors until it's gone
+#            if self.ultrasonicSensor.value() < 90:  # 90mm
+#                self.pauseMotorsAndStartLEDFlashing()
+#
+#                self.say("I am in danger!")
+#
+#                while self.ultrasonicSensor.value() < 90:
+#                    if self.button.enter or self.button.backspace:
+#                        Exception("Exit button was pressed.")
+#
+#                # while being broken indicates obstacle is gone
+#                self.unpauseMotorsAndStopLEDFlashing()
 
-                # if something is close to the sensor, stop motors until it's gone
-                if self.ultrasonicSensor.value() < 90:  # 90mm
-                    self.pauseMotorsAndStartLEDFlashing()
 
-                    self.say("I am in danger!")
+            # reverse the left wheel if the left colour sensor is colorToFollow
+            if self.colorSensorLeft.color == colorToFollow:
+                if self.leftMotor.motor.speed > 0:
+                    self.leftMotor.reverse()
+            else:
+                if self.leftMotor.motor.speed < 0:
+                    self.leftMotor.forward()
 
-                    while self.ultrasonicSensor.value() < 90:
-                        if self.button.enter or self.button.backspace:
-                            Exception("Exit button was pressed.")
+            # reverse the right wheel if the right colour sensor is colorToFollow
+            if self.colorSensorRight.color == colorToFollow:
+                if self.rightMotor.motor.speed > 0:
+                    self.rightMotor.reverse()
+            else:
+                if self.rightMotor.motor.speed < 0:
+                    self.rightMotor.forward()
 
-                    # while broken indicates obstacle is gone
-                    self.unpauseMotorsAndStopLEDFlashing()
+            # detect if next line color is on left side
+            if sideTurnIsOn == 'Left' and self.colorSensorLeft.color == nextLineColor:
+                if (self.leftMotor.motor.position - leftWheelRotationAtPreviousJunction) > 240 \
+                        and (self.rightMotor.motor.position - rightWheelRotationAtPreviousJunction) > 240:  # degrees
+                    if junctionsToIgnore > 0:
+                        junctionsToIgnore -= 1
+                        leftWheelRotationAtPreviousJunction = self.leftMotor.motor.position
+                        rightWheelRotationAtPreviousJunction = self.rightMotor.motor.position
 
-                # reverse the left wheel if the left colour sensor is colorToFollow
-                if self.colorSensorLeft.color == colorToFollow:
-                    self.l_motor_thread.reverse = True
-                else:
-                    self.l_motor_thread.reverse = False
+                    else:
+                        colorToFollow = nextLineColor
+                        self.leftMotor.pause()
+                        print("Waiting for Right sensor to find " + self.NUM_TO_COLOR[nextLineColor])
+                        while not self.colorSensorRight.color == nextLineColor:
+                            pass
+                        self.leftMotor.forward()
+                        return
 
-                # reverse the right wheel if the right colour sensor is colorToFollow
-                if self.colorSensorRight.color == colorToFollow:
-                    self.r_motor_thread.reverse = True
-                else:
-                    self.r_motor_thread.reverse = False
+            # detect if next line color is on right side
+            if sideTurnIsOn == 'Right' and self.colorSensorRight.color == nextLineColor:
+                if (self.leftMotor.motor.position - leftWheelRotationAtPreviousJunction) > 240 \
+                        and (self.rightMotor.motor.position - rightWheelRotationAtPreviousJunction) > 240:
+                    if junctionsToIgnore > 0:
+                        junctionsToIgnore -= 1
+                        leftWheelRotationAtPreviousJunction = self.leftMotor.motor.position
+                        rightWheelRotationAtPreviousJunction = self.rightMotor.motor.position
 
-                # ~100hz
-                sleep(0.01)
+                    else:
+                        colorToFollow = nextLineColor
+                        self.rightMotor.pause()
+                        print("Waiting for left sensor to find " + self.NUM_TO_COLOR[nextLineColor])
+                        while not self.colorSensorLeft.color == nextLineColor:
+                            pass
+                        self.rightMotor.forward()
+                        return
 
-                # detect next line color
-                if sideTurnIsOn == 'Left' and self.colorSensorLeft.color == nextLineColor \
-                        or sideTurnIsOn == 'Right' and self.colorSensorRight.color == nextLineColor:
-                    if (self.l_motor_thread.motor.position - leftWheelRotationAtPreviousJunction) > 240 \
-                            and (self.r_motor_thread.motor.position - rightWheelRotationAtPreviousJunction) > 240:  # degrees
-                        if junctionsToIgnore > 0:
-                            junctionsToIgnore -= 1
-                            leftWheelRotationAtPreviousJunction = self.l_motor_thread.motor.position
-                            rightWheelRotationAtPreviousJunction = self.r_motor_thread.motor.position
+            sleep(0.01) # 100 Hz
 
-                        else:
-                            colorToFollow = nextLineColor
-                            print("\nTurning! New color: " + self.NUM_TO_COLOR[colorToFollow])
-                            return
 
-                self.checkForSpeedChange()
-                colorToFollow = self.checkForColorChange(colorToFollow)
-
-        except Exception("Exit button was pressed."):
-            print("Button was pressed - Stopping motors.")
-            self.l_motor_thread.work = False
-            self.r_motor_thread.work = False
-            sleep(0.5)
+#            self.checkForSpeedChange()
+#            colorToFollow = self.checkForColorChange(colorToFollow)
 
     # accept a list of tuples containing the colors, and turnsides to follow
     def follow_lines(self, list_of_tuples):
@@ -98,23 +128,27 @@ class Line_Follower:
             self.follow_line(colorToFollow, nextLineColor, turnSide, junctionsToIgnore)
 
     def initialiseMotors(self, speed):
-        l_motor_thread = self.MotorThread("Left", "A")
-        l_motor_thread.setDaemon(True)  # stop thread when main thread ends
-        l_motor_thread.speed = speed
-        l_motor_thread.start()
+        print("Initialising motors: ")
 
-        r_motor_thread = self.MotorThread("Right", "D")
-        r_motor_thread.setDaemon(True)
-        r_motor_thread.speed = speed
-        r_motor_thread.start()
-        print("Starting motors: ")
-        return l_motor_thread, r_motor_thread
+        leftMotor = self.MotorThread("Left", "A")
+        leftMotor.setDaemon(True)  # stop thread when main thread ends
+        leftMotor.speed = speed
+        leftMotor.pause()
+        leftMotor.motor.polarity ='inversed'
+
+        rightMotor = self.MotorThread("Right", "D")
+        rightMotor.setDaemon(True)
+        rightMotor.speed = speed
+        rightMotor.pause()
+        rightMotor.motor.polarity = 'inversed'
+
+        leftMotor.start()
+        rightMotor.start()
+
+        return leftMotor, rightMotor
 
     class MotorThread(threading.Thread):
         def __init__(self, side, out):
-            self.work = True
-            self.pause = False
-            self.reverse = False
             self.side = side
             self.speed = 0
 
@@ -131,13 +165,16 @@ class Line_Follower:
         def run(self):
             print(self.side, "wheel is ready")
 
-            while self.work:
-                if self.pause:
-                    self.motor.run_direct(duty_cycle_sp=0)
-                elif self.reverse:
-                    self.motor.run_direct(duty_cycle_sp=-self.speed)
-                else:
-                    self.motor.run_direct(duty_cycle_sp=self.speed)
+        def reverse(self):
+            self.motor.run_forever(speed_sp=-self.speed)
+
+        def pause(self):
+            self.motor.run_forever(speed_sp=0)
+
+        def forward(self):
+            self.motor.run_forever(speed_sp=self.speed)
+
+        def stop(self):
             self.motor.stop()
 
     def initialiseLEDThread(self):
@@ -166,14 +203,14 @@ class Line_Follower:
                     sleep(0.300)  # 300ms
 
     def pauseMotorsAndStartLEDFlashing(self):
-        self.l_motor_thread.pause = True
-        self.r_motor_thread.pause = True
+        self.leftMotor.pause()
+        self.rightMotor.pause()
         self.led_thread.flashing = True
 
     def unpauseMotorsAndStopLEDFlashing(self):
         self.led_thread.flashing = False
-        self.l_motor_thread.pause = False
-        self.r_motor_thread.pause = False
+        self.leftMotor.forward()
+        self.rightMotor.forward()
 
     def say(self, message):
         Sound.set_volume(100)
@@ -187,14 +224,14 @@ class Line_Follower:
             self.decreaseSpeed()
 
     def decreaseSpeed(self):
-        self.l_motor_thread.speed -= 1
-        self.r_motor_thread.speed -= 1
-        print("\n" + "Left: " + str(self.l_motor_thread.speed) + "  -  Right: " + str(self.r_motor_thread.speed))
+        self.leftMotor.speed -= 1
+        self.rightMotor.speed -= 1
+        print("\n" + "Left: " + str(self.leftMotor.speed) + "  -  Right: " + str(self.rightMotor.speed))
 
     def increaseSpeed(self):
-        self.l_motor_thread.speed += 1
-        self.r_motor_thread.speed += 1
-        print("\n" + "Left: " + str(self.l_motor_thread.speed) + "  -  Right: " + str(self.r_motor_thread.speed))
+        self.leftMotor.speed += 1
+        self.rightMotor.speed += 1
+        print("\n" + "Left: " + str(self.leftMotor.speed) + "  -  Right: " + str(self.rightMotor.speed))
 
     def checkForColorChange(self, colorToFollow):
         # change line color to follow with left/right
--- EV3/Line_Follower_Tester.py
+++ EV3/Line_Follower_Tester.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 from Line_Follower import *
 
-lineFollower = Line_Follower()
+lineFollower = Line_Follower(200) # set speed to 30
 
-listOfLines = [['Green', 'White', 'Right', '0'],
-               ['White', 'Green', 'Left', '1'],
-               ['Green', 'Red', 'Right', '0'],
-               ['Red', 'White', 'Right', '0'],
-               ['White', 'Green', 'Left', '0']]
+listOfLines = [['Black', 'Red', 'Right', '0'],
+               ['Blue', 'Black', 'Right', '0'],
+               ['Black', 'Red', 'Right', '0'],
+               ['Red', 'Green', 'Left', '0']]
 
+print("Press enter to begin.")
+while not Button().enter:
+    lineFollower.checkForSpeedChange()
+
+print("Calling line follower.")
 lineFollower.follow_lines(listOfLines)
+
+# stop motors
+lineFollower.leftMotor.stop()
+lineFollower.rightMotor.stop()
+
+print("Done.")
+sleep(10)
